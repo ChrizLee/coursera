b35234aaba120455f87bd1058a6b7233d459d85e
liyx <liyx@cicc.com.cn>
2013-05-08 18:06:10 +0800

    assignment 2

--- data-science/assignment1/tweet_sentiment.py
+++ data-science/assignment1/tweet_sentiment.py
@@ -1,43 +1,65 @@
 import sys
 import json
 
-def hw():
-    print 'Hello, world!'
-
 def lines(fp):
     print str(len(fp.readlines()))
 
-# parse the setiment file and put score of word into a dictionary
-def score_term(term_file):
-	scores = {} # initialize an empty dictionary
-	for line in term_file:
-  		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-  		scores[term] = int(score)  # Convert the score to an integer.
+# parse the setiment file and put score of word and phrase into two dicts
+def parse_score(term_file):
+    scores = { 'word': {}, 'phrase': {} } # initialize an empty dictionary
+    print scores.items()
+    for line in term_file:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        if ' ' in term:
+            scores['phrase'][term] = int(score)
+        else:
+            scores['word'][term] = int(score)
 
-	#print scores.items() # Print every (term, score) pair in the dictionary
-	return scores;
+    #print len(scores['phrase'].items())
+    #print len(scores['word'].items())
 
-def calc_score(scores, text):
-	
+    return scores
 
-def get_tweets(tweet_file):
-	tweets = json.load(tweet_file)
-	print len(tweets)
-	for t in tweets:
-		text = 
+# get score of a given text
+def get_score(text, scores):
+    score = 0
+    # add word scores first
+    for word in  text.split(' '):
+        word = word.lower()
+        if word in scores['word']:
+            score += scores['word'][word]
 
+    # add phrase scores and, if duplicated with word, substract the word score
+    for phrase, phrase_score in scores['phrase'].iteritems():
+        if phrase in text:
+            score += phrase_score
+            for word_in_phrase in phrase.split(' '):
+                if word_in_phrase in scores['word']:
+                    score -= scores['word'][word_in_phrase]
+
+    return score
+
+def cal_senti_score(tweet_file, term_score):
+    for line in tweet_file:
+        tweet = json.loads(line)
+        if not 'text' in tweet:
+            print 0
+        else:
+            print '%d' % get_score(tweet['text'], term_score)
+
+        
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
     #lines(sent_file)
     #lines(tweet_file)
 
     try: 
-    	term_score = score_term(sent_file)
+        term_score = parse_score(sent_file)
+        cal_senti_score(tweet_file, term_score)
     finally:
-	    sent_file.close()
-	    tweet_file.close()
+        sent_file.close()
+        tweet_file.close()
 
 if __name__ == '__main__':
     main()
