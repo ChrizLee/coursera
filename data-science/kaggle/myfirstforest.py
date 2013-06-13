8b5d4451e464b7e46ffaba60a39ae7a7d6770c2e
liyx <liyx@cicc.com.cn>
2013-06-13 17:59:32 +0800

    add cabin count as a feature

--- data-science/kaggle/myfirstforest.py
+++ data-science/kaggle/myfirstforest.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import csv as csv
+import feature
 from sklearn import cross_validation
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
@@ -13,27 +14,9 @@ def main():
         train_data.append(row)  # adding each row to the data variable
     train_data = np.array(train_data)  # Then convert from a list to an array
     
-    # I need to convert all strings to integer classifiers:
-    # Male = 1, female = 0:
-    train_data[train_data[0::, 3] == 'male', 3] = 1
-    train_data[train_data[0::, 3] == 'female', 3] = 0
-    # embark c=0, s=1, q=2
-    train_data[train_data[0::, 10] == 'C', 10] = 0
-    train_data[train_data[0::, 10] == 'S', 10] = 1
-    train_data[train_data[0::, 10] == 'Q', 10] = 2
+    train_data = feature.extract(train_data, "train")
     
-    # I need to fill in the gaps of the data and make it complete.
-    # So where there is no price, I will assume price on median of that class
-    # Where there is no age I will give median of all ages
     
-    # All the ages with no data make the median of the data
-    train_data[train_data[0::, 4] == '', 4] = np.median(train_data[train_data[0::, 4]\
-                                               != '', 4].astype(np.float))
-    # All missing ebmbarks just make them embark from most common place
-    train_data[train_data[0::, 10] == '', 10] = np.round(np.mean(train_data[train_data[0::, 10]\
-                                                       != '', 10].astype(np.float)))
-    
-    train_data = np.delete(train_data, [2, 7, 9], 1)  # remove the name data, cabin and ticket
     # I need to do the same with the test data now so that the columns are in the same
     # as the training data
     
@@ -44,29 +27,7 @@ def main():
         test_data.append(row)  # adding each row to the data variable
     test_data = np.array(test_data)  # Then convert from a list to an array
     
-    # I need to convert all strings to integer classifiers:
-    # Male = 1, female = 0:
-    test_data[test_data[0::, 2] == 'male', 2] = 1
-    test_data[test_data[0::, 2] == 'female', 2] = 0
-    # ebark c=0, s=1, q=2
-    test_data[test_data[0::, 9] == 'C', 9] = 0  # Note this is not ideal, in more complex 3 is not 3 tmes better than 1 than 2 is 2 times better than 1
-    test_data[test_data[0::, 9] == 'S', 9] = 1
-    test_data[test_data[0::, 9] == 'Q', 9] = 2
-    
-    # All the ages with no data make the median of the data
-    test_data[test_data[0::, 3] == '', 3] = np.median(test_data[test_data[0::, 3]\
-                                               != '', 3].astype(np.float))
-    # All missing ebmbarks just make them embark from most common place
-    test_data[test_data[0::, 9] == '', 9] = np.round(np.mean(test_data[test_data[0::, 9]\
-                                                       != '', 9].astype(np.float)))
-    # All the missing prices assume median of their respectice class
-    for i in xrange(np.size(test_data[0::, 0])):
-        if test_data[i, 7] == '':
-            test_data[i, 7] = np.median(test_data[(test_data[0::, 7] != '') & \
-                                                 (test_data[0::, 0] == test_data[i, 0])\
-                , 7].astype(np.float))
-    
-    test_data = np.delete(test_data, [1, 6, 8], 1)  # remove the name data, cabin and ticket
+    test_data = feature.extract(test_data, "test")
     
     # The data is now ready to go. So lets train then test!
     
@@ -79,18 +40,20 @@ def main():
     scores = cross_validation.cross_val_score(forest, cv_data, cv_target, cv=5)    
     print "Cross Validation for random forest, scores: " + str(np.mean(scores))
     
+    '''
     C_list = [0.1, 0.3, 1, 3, 10, 30, 100, 300, 1000]
     for c in C_list:
         svm_clf = svm.SVC(C=c)
         scores = cross_validation.cross_val_score(svm_clf, cv_data, cv_target, cv=5)
         print "Cross Validation for SVM, scores with C = %f : %f" % (c, np.mean(scores))
+    '''     
     
     
-    
-    '''
     forest = forest.fit(cv_data, cv_target)
     
     print 'Predicting'
+    for row in test_data:
+        print row
     output = forest.predict(test_data)
     
     open_file_object = csv.writer(open("csv/submission.csv", "wb"))
@@ -103,7 +66,7 @@ def main():
         row.insert(0, output[i].astype(np.uint8))
         open_file_object.writerow(row)
         i += 1
-    '''
+    
     print "Done"
     
 if __name__ == "__main__":
